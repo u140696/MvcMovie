2232354e20d0efd21caaff7c54b3407bc8e263cf
Urvish <w0874636@myscc.ca>
2025-02-05 18:28:32 -0500

    Initial commit - MvcMovie project

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+        : base(options)
+        { }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
